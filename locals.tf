81c9ea9b457217a24cdac69fe46b87830a54470c
Simon Lynch <simon.lynch@hashicorp.com>
2023-09-19 10:57:22 +1000

    update

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   configPath =  path.module
   default_resource_tags = {
 
-    moduleVersion = join(" ", ["pcs-module-name", jsondecode(file("${path.module}/.cz.json")).commitizen.version])
+    moduleVersion = join(" ", ["pcs-module-name", jsondecode(file("${path.module}/cz.json")).commitizen.version])
 
   }
 
